d353b9e3c079dfcd0fcb85ae9adfb50a6818e7b7
Pedro <pedro_fernandes@live.com>
2017-01-02 23:24:08 +0000

    dsfadsf

---
+++ augmentation.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <sstream>
+#include <time.h>
+#include <stdio.h>
+#include <fstream>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/aruco.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+
+
+	system("PAUSE");
+	return 0;
+}
